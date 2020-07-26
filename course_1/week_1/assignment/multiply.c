43eccd7e8044301b1f9e0f8fe635469b74ad6628
jwschroeder330 <jschroeder@linkmedia360.com>
2020-07-26 11:28:33 -0400

    checkpoint

--- course_1/week_1/assignment/multiply.c
+++ course_1/week_1/assignment/multiply.c
@@ -4,6 +4,55 @@
 #include <math.h>
 
 
+
+char * splitFirstHalf(char * x, int xlen) {
+    int xIdx = xlen > 1 ? xlen / 2 : 1;
+    char * half = malloc(sizeof(half) * xIdx);
+    if(xlen > 1) {
+        strncpy(half, x, xIdx);
+    } else {
+        strcpy(half, "0");
+    }
+    return half;
+}
+
+char * splitLastHalf(char * x, int xlen) {
+    int xIdx = xlen > 1 ? xlen / 2 : 1;
+    char * half = malloc(sizeof(half) * xIdx);
+    if(xIdx % 2 == 0) {
+        strncpy(half, &x[xIdx - 1], xIdx);
+    } else {
+        if(xlen == 1) {
+            strncpy(half, &x[xIdx - 1], xIdx);
+        } else {
+            strncpy(half, &x[xIdx], xIdx);
+        }
+    }
+    return half;
+}
+
+
+char * splitInput(char * input, int index, int len) {
+    char * split = malloc(sizeof(input));
+    if (len == 1 && index == 0) {
+        // zero-pad single length inputs that need to be split
+        strcpy(split, "0");
+    }
+    else if (len == 1 && index == 1) {
+        // take the single input for the second half of the split
+        strcpy(split, input);
+    } else {
+        // split using integer (floor) division
+        int splitIdx = len / 2;
+        if (index == 0) {
+            strncpy(split, input, splitIdx);
+        } else {
+            strncpy(split, &input[splitIdx - 1], len - splitIdx);
+        }
+    }
+    return split;
+}
+
 /**
  * karatsuba
  * ``````````````````````````````````````````````````````````````````````````````````````````````
@@ -19,67 +68,30 @@
 int karatsuba(char * x, char * y) {
     
     // get the length of each input str, and convert them to integers
-    int lenx = strlen(x);
-    int leny = strlen(y);
+    int lenX = strlen(x);
+    int lenY = strlen(y);
     
-    int xInt = atoi(x);
-    int yInt = atoi(y);
+    int nMax = (lenX > lenY) ? lenX : lenY;
 
     // base condition: if both arguments are size 1, then simply multiply and return
-    if(lenx <= 1 && leny <= 1) {
-        if(lenx == 0) {
-            return yInt;
-        } else if (leny == 0) {
-            return xInt;
-        } else {
-            return xInt * yInt;
-        }
+    if(lenX == 1 && lenY == 1) {
+        return atoi(x) * atoi(y);
     }
-    printf("RECURSION\n");
-    // recursive condition
-    // determine max input length between x and y
-    int nMax = (lenx >= leny) ? lenx : leny;
-
-    // determine if each input lengths are even
-    int xEven = lenx % 2 == 0;   
-    int yEven = leny % 2 == 0;
     
-    // get the floor midpoint based on integer division
-    int xIdx = lenx / 2;
-    int yIdx = leny / 2;
+    char * a = splitInput(x, 0, lenX);
+    char * b = splitInput(x, 1, lenX);
+    char * c = splitInput(y, 0, lenY);
+    char * d = splitInput(y, 1, lenY);
 
-    printf("lenx: %d, xIdx: %d, leny: %d, yIdx: %d\n", lenx, xIdx, leny, yIdx);
-
-    // split each input string in half
-    char * x1 = malloc(sizeof(x1) * nMax);
-    char * x2 = malloc(sizeof(x2) * nMax);
-    char * y1 = malloc(sizeof(y1) * nMax);
-    char * y2 = malloc(sizeof(y2) * nMax);
-    
-    // first halves are always to the floor idx
-    x1 = strncpy(x1, x, xIdx);
-    y1 = strncpy(y1, y, yIdx);
-    
-    // second halves depend on whether the input lengths are even or not
-    if(xEven == 1) {
-        x2 = strncpy(x2, &x[xIdx], xIdx);
-    } else {
-        x2 = strncpy(x2, &x[xIdx], xIdx + 1);
-    }
-    if(yEven == 1) {
-        y2 = strncpy(y2, &y[yIdx], yIdx);
-    } else {
-        y2 = strncpy(y2, &y[yIdx], yIdx + 1);
-    }
-    printf("x: %s, y: %s, x1: %s, x2: %s, y1: %s, y2: %s\n", x, y, x1, x2, y1, y2);
+    printf("a: %s, b: %s, c: %s, d: %s\n", a, b, c, d);
 
     // subproblem one and two
-    int resultA = karatsuba(x1, y1);
-    int resultB = karatsuba(x2, y2);
-    
+    int resultA = karatsuba(a, c);
+    int resultB = karatsuba(b, d);
+
     // prepare for the third subproblem - add, cast to str, and recursive call
-    int z1 = atoi(x1) + atoi(x2);
-    int z2 = atoi(y1) + atoi(y2);
+    int z1 = atoi(a) + atoi(b);
+    int z2 = atoi(c) + atoi(d);
 
     char * z1Str = malloc(sizeof(z1Str) * nMax);
     char * z2Str = malloc(sizeof(z2Str) * nMax);
@@ -87,20 +99,25 @@ int karatsuba(char * x, char * y) {
     sprintf(z1Str, "%d", z1);
     sprintf(z2Str, "%d", z2);
 
+    // printf("z1: %s, z2: %s \n", z1Str, z2Str);
     int resultC = karatsuba(z1Str, z2Str) - resultA - resultB;
+    
+    // printf("ra: %d, rb: %d, rc: %d \n", resultA, resultB, resultC);
 
     // free up now unused memory
-    free(x1);
-    free(x2);
-    free(y1);
-    free(y2);
+    free(a);
+    free(b);
+    free(c);
+    free(d);
     free(z1Str);
     free(z2Str);
 
     // get the products of 10**n/2 and 10**n
-    int nPowHalf = pow(10, nMax / 2);
+    int nMaxHalf = nMax % 2 == 0 ? nMax / 2 : (nMax + 1) / 2;
+    int nPowHalf = pow(10, nMaxHalf);
     int nPow = pow(10, nMax);
-
+    printf("nMax: %d, nMaxHalf: %d, nPowHalf: %d, nPow: %d\n", nMax, nMaxHalf, nPowHalf, nPow);
+    
     // return using Karatsuba's insight - this allows us to use 3 subproblems instead of 4
     return (resultA * nPow) + (resultC * nPowHalf) + resultB;
 }
