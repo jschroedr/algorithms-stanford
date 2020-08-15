d83d2df99f03b7481a9a6a38e9e7ea88d42af5de
jwschroeder330 <jschroeder@linkmedia360.com>
2020-08-15 17:26:20 -0400

    assn documentation

--- course_1/week_2/assignment-c/SortAndCount/inversion.c
+++ course_1/week_2/assignment-c/SortAndCount/inversion.c
@@ -1,7 +1,21 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/* 
+ * @title: Array Inversion Source File
+ * @author: Jake Schroeder 
+ * @license:     
+ *      This file is part of "Array Inversion Computation in C" 
+ *  
+ *      "Array Inversion Computation in C" is free software: you can redistribute it and/or modify
+ *      it under the terms of the GNU General Public License as published by
+ *      the Free Software Foundation, either version 3 of the License, or
+ *      (at your option) any later version.
+ * 
+ *      "Array Inversion Computation in C" is distributed in the hope that it will be useful,
+ *      but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *      GNU General Public License for more details.
+ * 
+ *      You should have received a copy of the GNU General Public License
+ *      along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 #include <stdio.h>
 #include <stdlib.h>
@@ -9,6 +23,23 @@
 #include <gmp.h>
 
 
+/**
+ * getMergeItem
+ * ============================================================================
+ * As long as a is not out of values
+ * Gets value at aCount in source array a, converts to an integer and returns
+ * 
+ * @internal
+ *      here again is our assumption that the user's input array of strings 
+ *      will all have values within the bounds of an integer
+ * 
+ *      see also: setMergeItem
+ * 
+ * @param a
+ * @param aLen
+ * @param aCount
+ * @return 
+ */
 int getMergeItem(char ** a, int aLen, int aCount) {
     if(aCount < aLen) {
         char * aStr = a[aCount];
@@ -17,7 +48,22 @@ int getMergeItem(char ** a, int aLen, int aCount) {
     return 0;
 }
 
-
+/**
+ * setMergeItem
+ * ============================================================================
+ * Sets the value of the destination array d at dCount to the string value of a 
+ * 
+ * @internal 
+ *      note that all numbers that are representable by int will fit in a 
+ *      12-char array
+ * 
+ *      in this way, we assume the users input array of strings contains 
+ *      only values within the bounds of a traditional integer
+ * 
+ * @param d; destination array
+ * @param dCount; index at destination array where a should be set
+ * @param a; integer value to be converted and set in d
+ */
 void setMergeItem(char ** d, int dCount, int a) {
     // all numbers that are representable by int will fit in a 12-char array
     int maxIntChars = 12;
@@ -27,29 +73,31 @@ void setMergeItem(char ** d, int dCount, int a) {
 }
 
 
-int sortAscending(const void * p1, const void * p2) {
-    return *(const int *)p1 - *(const int *)p2;
-}
-
-
 /**
+ * mergeAndCountSplitInv
+ * ============================================================================
+ * Calculate split inversions using the "trick" of calculating differential
+ * inversions (documented below).
  * 
- * @internal: d must be allocated large enough to hold char * of (bLen + cLen length)
- * 
- * @param b
- * @param bLen
- * @param c
- * @param cLen
- * @param d
- * @return 
+ * @param b; first slice of original input array, sorted
+ * @param bLen; length of b
+ * @param c; second slice of original input array, sorted
+ * @param cLen; length of c
+ * @param d; destination array where full sorted outcome will be set
+ * @param z; inversion result; any discovered inversions will be added to this
  */
 void mergeAndCountSplitInv(char ** b, int bLen, char ** c, int cLen, char ** d, mpz_t z) {
     
+    // initialize counters for b, c and d: i, j, k
     int i = 0;
     int j = 0;
     int k = 0;
+    
+    // calculate the final length of d: n
     int n = bLen + cLen;
     
+    // initialize a bignum int for bLen so we can properly compute total 
+    // inversions as z
     mpz_t mpzBLen;
     mpz_init(mpzBLen);
     mpz_add_ui(mpzBLen, mpzBLen, bLen);
@@ -62,41 +110,66 @@ void mergeAndCountSplitInv(char ** b, int bLen, char ** c, int cLen, char ** d, 
         int x = getMergeItem(b, bLen, i);
         int y = getMergeItem(c, cLen, j);
         
+        // NON-INVERSION CASES
         // if either:
             // b is equal to c (prefer left array)
             // b input is smaller than c OR
             // c is out of inputs to copy
         if ((x < y || x == y || j == cLen) && i < bLen ) {
+            
+            // set index k of d to value of b (x)
             setMergeItem(d, k, x);
-        
             i++;
             
+        // INVERSION CASES
         // if either:
            // c input is smaller than b OR
            // b is out of inputs to copy
         } else if ((x > y || i == bLen) && j < cLen) {
-            setMergeItem(d, k, y);
             
+            // set index k of d to value of c (y)
+            setMergeItem(d, k, y);
             j++;
             
-            // inversions increment by number of elements left in b
-            // when the item was copied from c
+            // initialize a bignum int for computing the differential 
+            // inversions
             mpz_t diff;
             mpz_init(diff);
             
+            // calculate differential inversions as 
+            // 
+            // number of elements left in b
+            // when the item was copied from c 
+            //
+            // aka: (b's length minus b's counter)
             mpz_sub_ui(diff, mpzBLen, i);
+            
+            // add differential inversions to total inversions: z
             mpz_add(z, z, diff);
             
+            // clear the memory allocated for the differential inversion
+            // count
+            // @internal: you may be able to optimize this so we don't have
+            //      to clear for each inversion instance
             mpz_clear(diff);
         }
+        
+        // always increment d's counter
         k ++;
     }
     
-    // return the number of split inversions as z's new value
     return;
 }
 
-
+/**
+ * copyArray
+ * ============================================================================
+ * Helper function to copy all members of x into a y given len
+ * 
+ * @param x; input array to copy
+ * @param len; number of input array elements to copy, starting at x[0]
+ * @return malloc'd char ** pointer to x-copy, y
+ */
 char ** copyArray(char ** x, int len) {
     char ** y = NULL;
     for(int i = 0; i < len; i ++) {
@@ -109,8 +182,27 @@ char ** copyArray(char ** x, int len) {
     return y;
 }
 
-
+/**
+ * allocateSlice
+ * ============================================================================
+ * Helper function to determine how to use copyArray, given whether this is 
+ * the first or second slice (idx), and the nature of len (odd or even)
+ * 
+ * @param array; input array of strings, see "sortAndCount"
+ * @param len; length of given array, see "sortAndCount"
+ * @param idx; whether this is the first (0) or second (1) slice
+ * @param destLen; initialized integer pointer we can set the
+ *      resulting length to
+ * @return 
+ */
 char ** allocateSlice(char ** array, int len, int idx, int * destLen) {
+    
+    // initialize and set the point where the first array (idx = 0) should be 
+    // copied to and where the second array's (idx = 1) copy should begin
+    //
+    // dynamically set the length pointer based on these conditions, keeping
+    // in mind special handling required for the second half of odd-len
+    // string arrays (see: midpoint + 1)
     int midpoint;
     if (len % 2 == 0) {
         midpoint = len / 2;
@@ -123,9 +215,7 @@ char ** allocateSlice(char ** array, int len, int idx, int * destLen) {
             midpoint = len - (len / 2) - 1;
             *destLen = midpoint + 1;
         }
-    }
-    
-                
+    }  
 
     if (idx == 0) {
         // if we want the first slice, we simply need to return the array (array[0])
@@ -137,6 +227,22 @@ char ** allocateSlice(char ** array, int len, int idx, int * destLen) {
 }
 
 
+/**
+ * sortAndCount
+ * ============================================================================
+ * The main method of the inversion module
+ * 
+ * For a given array of strings, char ** array, of a given length int len
+ * divide and conquer to compute the total number of inversions, stored as 
+ * mpz_t result
+ * 
+ * @param array; input array of strings, properly null terminated and clear
+ *      of any non-numeric input (no \n)
+ * @param len; length of given array exactly equalling number of strings 
+ *      contained therein
+ * @param result; bignum integer, initialized, which the resulting number of 
+ *      inversions will be added to
+ */
 void sortAndCount(char ** array, int len, mpz_t result) {
     // base condition - return no inversions if the length is one
     // @internal: since mpz_init sets value to 0, we do not have to worry
--- course_1/week_2/assignment-c/SortAndCount/main.c
+++ course_1/week_2/assignment-c/SortAndCount/main.c
@@ -1,16 +1,32 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /* 
- * File:   main.c
- * Author: jake
- *
- * Created on August 9, 2020, 5:23 PM
+ * @title: Array Inversion Computation in C
+ * @author: Jake Schroeder 
+ * @license:      
+ *      This program is free software: you can redistribute it and/or modify
+ *      it under the terms of the GNU General Public License as published by
+ *      the Free Software Foundation, either version 3 of the License, or
+ *      (at your option) any later version.
+ * 
+ *      This program is distributed in the hope that it will be useful,
+ *      but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *      GNU General Public License for more details.
+ * 
+ *      You should have received a copy of the GNU General Public License
+ *      along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ * 
+ * @internal:
+ *      
+ *      The intent of this code is to find the number of index inversions
+ *      in a a file of one-integer-per-line values named "numbers.txt"
+ *      stored in a relative directory "data"
+ * 
+ *      Please also note that this assignment depends on the use of the 
+ *      GNU BigNum library to compute large integer values that exceed
+ *      the maximums of built-ins.
+ * 
+ *      Tested on Ubuntu 20.04, GCC 9.3.0
  */
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -18,13 +34,15 @@
 #include "inversion.h"
 
 
-/*
- * 
- */
 int main() {
+    
+    // set the file name for the input data
+    // @internal: decide to read in the production or test file here
     char * fileName = "data/numbers.txt";
     // char * fileName = "data/numbers_test_10_16.txt";
     
+    // set the file pointer and attempt to read
+    // let the error propagate if we cannot access the file for any reason
     FILE * fp;
     fp = fopen(fileName, "r");
     if(fp == NULL) {
@@ -32,6 +50,8 @@ int main() {
         return EXIT_FAILURE;
     }
     
+    // read each line in the input file and store in lineArray
+    // keeping track of lineCount along the way
     size_t len = 0;
     size_t lineCount = 1;
     int nread;
@@ -39,6 +59,8 @@ int main() {
     char ** lineArray = NULL;
     while((nread = getline(&line, &len, fp)) != -1) {
         int lineLen = strlen(line);
+        // here we check if atoi() failes
+        // @internal it is assumed that all values in numbers.txt are non-zero
         if (atoi(line) == 0) {
             break;
         }
@@ -52,21 +74,20 @@ int main() {
         }
         lineCount ++;
     }
+    // close the file
     fclose(fp);
     
+    // initialize where the total number of inversions will be stored
     mpz_t result;
     mpz_init(result);
     
+    // call sortAndCount and compute inversions
+    // @internal note we use lineCount -1 because the last line is typically 
+    //      an empty string
     sortAndCount(lineArray, (int)(lineCount - 1), result);
-
-    /*
-    for(int i = 0; i < (lineCount - 1); i ++) {
-        printf("%s\n", lineArray[i]);
-    }
-    */
     
+    // convert the bignum result to a string for display in stdout
     char * resultStr = mpz_get_str(NULL, 10, result);
-    
     printf("\nTotal Inversions is %s", resultStr);
     return (EXIT_SUCCESS);
 }
